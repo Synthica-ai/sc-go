061381f31e8497f3328ebe5553d309d2e618cc36
Goginet <gogi.soft.gm@gmail.com>
2023-04-30 02:08:23 +0300

    Revert cors middleware

--- server/middleware/auth.go
+++ server/middleware/auth.go
@@ -22,19 +22,6 @@ const (
 	AuthLevelSuperAdmin
 )
 
-func (m *Middleware) CorsMiddleware() func(next http.Handler) http.Handler {
-	return func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			w.Header().Set("Access-Control-Allow-Origin", "*")
-			w.Header().Set("Access-Control-Allow-Methods", "*")
-			w.Header().Set("Access-Control-Allow-Headers", "*")
-			w.Header().Set("Access-Control-Allow-Credentials", "true")
-
-			next.ServeHTTP(w, r)
-		})
-	}
-}
-
 // Enforces authorization at specific level
 func (m *Middleware) AuthMiddleware(level AuthLevel) func(next http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
